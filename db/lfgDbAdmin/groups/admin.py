5d32721b8f5c2c33e159ee59e36e2fd608e30f8f
bessonov <sergebessonov@gmail.com>
2018-06-23 19:09:30 +0500

    db models fixes

--- db/lfgDbAdmin/groups/admin.py
+++ db/lfgDbAdmin/groups/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Group, PersonsType, Person
+from .models import Group, PersonsType, Person, Section
 
 admin.site.register(Group)
 admin.site.register(PersonsType)
 admin.site.register(Person)
+admin.site.register(Section)
--- db/lfgDbAdmin/groups/models.py
+++ db/lfgDbAdmin/groups/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Group(models.Model):
-    name = models.TextField(max_length=512)
-    section = models.TextField(max_length=512, blank = True)
+    name = models.TextField(max_length=256)
+    description = models.TextField(max_length=512, blank = True)
     status = models.IntegerField(blank = True)
+    section = models.ForeignKey('Section', blank = True, null = True, on_delete=models.SET_NULL)
     persons = models.ManyToManyField('PersonsType', blank = True)
     created_on = models.DateTimeField(auto_now_add=True)
 
@@ -14,19 +15,26 @@ class Group(models.Model):
 class PersonsType(models.Model):
     type = models.TextField(max_length=1024)
     groups = models.ManyToManyField('Group', blank = True)
-    section = models.TextField(max_length=512, blank = True)
+    section = models.ManyToManyField('Section', blank = True)
 
     def __str__(self):
         return self.type
 
 class Person(models.Model):
-    tel_id = models.TextField(max_length=512)
-    name = models.TextField(max_length=512)
-    l_name = models.TextField(max_length=512)
+    tel_id = models.TextField(max_length=256)
+    name = models.TextField(max_length=256)
+    l_name = models.TextField(max_length=256)
     type = models.ForeignKey('PersonsType', blank = True, null = True, on_delete=models.SET_NULL)
-    section = models.TextField(max_length=512, blank = True)
+    section = models.ForeignKey('Section', blank = True, null = True, on_delete=models.SET_NULL)
     status = models.IntegerField()
     cur_group = models.ForeignKey('Group', blank = True, null = True, on_delete=models.SET_NULL)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name + ' ' + self.l_name
+
+class Section(models.Model):
+    title = models.TextField(max_length=256)
+
+    def __str__(self):
+        return self.title
